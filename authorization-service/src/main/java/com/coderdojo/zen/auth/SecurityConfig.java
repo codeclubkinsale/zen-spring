21b1cec595a05126224f17d2026ba29096226a0e
anthonygnolan <anthonygnolan@icloud.com>
2025-09-03 18:21:47 +0100

    chore: use original layout

--- authorization-service/src/main/java/com/coderdojo/zen/auth/SecurityConfig.java
+++ authorization-service/src/main/java/com/coderdojo/zen/auth/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.coderdojo.zen.auth;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.util.UUID;
-
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
-
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -46,25 +44,12 @@ public class SecurityConfig {
    * Sole constructor. (For invocation by subclass
    * constructors, typically implicit.).
    */
-  SecurityConfig() { /* Default Constructor */ }
-
-  /**
-   * Javadoc.
-   *
-   * @return Example
-   */
-  private static KeyPair generateRsaKey() {
-    KeyPair keyPair;
-    KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-    keyPairGenerator.initialize(2048);
-    keyPair = keyPairGenerator.generateKeyPair();
-    return keyPair;
+  SecurityConfig() { /* Default Constructor */
   }
 
   /**
    * Javadoc.
    *
-   * @param http Example
    * @return Example
    * @throws Exception Example
    */
@@ -168,6 +153,23 @@ public class SecurityConfig {
         .build();
     JWKSet jwkSet = new JWKSet(rsaKey);
     return new ImmutableJWKSet<>(jwkSet);
+  }
+
+  /**
+   * Javadoc.
+   *
+   * @return Example
+   */
+  private static KeyPair generateRsaKey() {
+    KeyPair keyPair;
+    try {
+      KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+      keyPairGenerator.initialize(2048);
+      keyPair = keyPairGenerator.generateKeyPair();
+    } catch (Exception ex) {
+      throw new IllegalStateException(ex);
+    }
+    return keyPair;
   }
 
   /**
