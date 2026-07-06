7a012b57a60054f8e23665b1386737fec0cf09b3
Anthony Nolan <anthonygnolan@icloud.com>
2026-07-06 02:12:54 +0100

    feature(belt-service): upgrade to Java 25 and SpringBoot 4
    Ref #1
    
    Signed-off-by: Anthony Nolan <anthonygnolan@icloud.com>

--- belt-service/src/test/java/com/coderdojo/zen/belt/BeltControllerTest.java
+++ belt-service/src/test/java/com/coderdojo/zen/belt/BeltControllerTest.java
@@ -21,8 +21,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -54,7 +54,7 @@ class BeltControllerTest {
    * Javadoc.
    */
   @Autowired
-  private ObjectMapper objectMapper;
+  private ObjectMapper objectMapper = new ObjectMapper();
 
   /**
    * Sole constructor. (For invocation by subclass
